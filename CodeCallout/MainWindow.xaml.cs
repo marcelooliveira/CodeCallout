a7a3bb7b9d836affd1d97220c138a77fa5c95d0d
marcelooliveira <mclricardo@gmail.com>
2017-02-22 14:13:26 -0300

    .

---
+++ CodeCallout/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace CodeCallout
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        readonly DispatcherTimer timer;
+        public MainWindow()
+        {
+            InitializeComponent();
+            timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromMilliseconds(500);
+            timer.Tick += Timer_Tick;
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            timer.Stop();
+            try
+            {
+                img.Source = new BitmapImage(
+                    new Uri(string.Format(@"\images\{0}.png", txt.Text.ToLower()), UriKind.Relative));
+            }
+            catch { }
+        }
+
+        protected override void OnActivated(EventArgs e)
+        {
+            base.OnActivated(e);
+            txt.Focus();
+        }
+
+        private void txt_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            timer.Stop();
+            timer.Start();
+        }
+    }
+}
